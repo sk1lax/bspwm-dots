88b617f5a36c8b7f11d04166ea2504011fbf9421
skilax <132996178+sk1lax@users.noreply.github.com>
2025-01-19 23:31:58 +0300

    Update brightness.sh

--- config/bspwm/bin/brightness.sh
+++ config/bspwm/bin/brightness.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-#▗▖    ▄▄▄ ▄    ▐▌    ■  ▄▄▄▄  ▗▞▀▚▖ ▄▄▄  ▄▄▄      ▄▄▄ ▗▞▀▘ ▄▄▄ ▄ ▄▄▄▄     ■
-#▐▌   █    ▄    ▐▌ ▗▄▟▙▄▖█   █ ▐▛▀▀▘▀▄▄  ▀▄▄      ▀▄▄  ▝▚▄▖█    ▄ █   █ ▗▄▟▙▄▖
-#▐▛▀▚▖█    █    ▐▛▀▚▖▐▌  █   █ ▝▚▄▄▖▄▄▄▀ ▄▄▄▀     ▄▄▄▀     █    █ █▄▄▄▀   ▐▌
-#▐▙▄▞▘     █ ▗▄▖▐▌ ▐▌▐▌                                         █ █       ▐▌
-#           ▐▌ ▐▌    ▐▌                                           ▀       ▐▌
-#            ▝▀▜▌
-#           ▐▙▄▞▘			https://github.com/sk1lax/bspwm-dots
+#    __         _       __    __                                         _       __
+#   / /_  _____(_)___ _/ /_  / /_____  ___  __________   _______________(_)___  / /_
+#  / __ \/ ___/ / __ `/ __ \/ __/ __ \/ _ \/ ___/ ___/  / ___/ ___/ ___/ / __ \/ __/
+# / /_/ / /  / / /_/ / / / / /_/ / / /  __(__  |__  )  (__  ) /__/ /  / / /_/ / /_
+#/_.___/_/  /_/\__, /_/ /_/\__/_/ /_/\___/____/____/  /____/\___/_/  /_/ .___/\__/
+#             /____/                                                  /_/
+#							https://github.com/sk1lax/bspwm-dots
 # 								Script to manage brightness
-# Get br
+
+# Get brightness
 get_brightness() {
 	brightness=$(echo $(brightnessctl info | grep "%" | cut -d ' ' -f 4))
 	echo "$brightness"
 }
 
-# Increase br
+# Increase brightness
 up_brightness() {
 	brightnessctl s 5+% && dunstify -u low --replace=69 -i /usr/share/icons/Papirus/22x22/panel/brightness-high-symbolic.svg "Brightness : $(get_brightness)"
 }
 
-# Decrease br
+# Decrease brightness
 down_brightness() {
 	brightnessctl s 5-% && dunstify -u low --replace=69 -i /usr/share/icons/Papirus/22x22/panel/brightness-symbolic.svg "Brightness : $(get_brightness)"
 }
